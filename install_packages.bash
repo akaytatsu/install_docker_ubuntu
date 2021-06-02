69b0efad5425aee05755061a7d4b60d3499bc21a
Thiago Freitas <thiagosistemas3@gmail.com>
2021-06-02 17:08:55 -0300

    corrçeão

--- install_packages.bash
+++ install_packages.bash
@@ -1,6 +1,6 @@
 sudo apt update -y
 sudo apt install software-properties-common -y
-sudo apt install -y build-essential git curl python3 python3-pip
+sudo apt install -y build-essential git curl python3 python3-pip python
 sudo snap install code --classic
 sudo snap install spotify
 sudo snap install sublime-text --classic
@@ -12,3 +12,5 @@ sudo snap install teams
 curl "https://s3.amazonaws.com/aws-cli/awscli-bundle-1.16.312.zip" -o "awscli-bundle.zip"
 unzip awscli-bundle.zip
 sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+rm -rf ./awscli-bundle
+rm awscli-bundle.zip
