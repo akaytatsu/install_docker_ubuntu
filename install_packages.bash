175213297d7805ca4a71c28524b29ca18ecb7e83
Thiago Freitas <thiagosistemas3@gmail.com>
2021-06-02 17:04:46 -0300

    melhprias

--- install_packages.bash
+++ install_packages.bash
@@ -1,5 +1,6 @@
 sudo apt update -y
-sudo apt install -y build-essential git curl
+sudo apt install software-properties-common
+sudo apt install -y build-essential git curl python3 python3-pip
 sudo snap install code --classic
 sudo snap install spotify
 sudo snap install sublime-text --classic
