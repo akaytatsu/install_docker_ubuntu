490b4dbf3429d96ac761d38488ecb9ae4958c41b
Thiago Freitas <thiagosistemas3@gmail.com>
2021-06-02 17:11:18 -0300

    corrçẽao

--- install_docker.bash
+++ install_docker.bash
@@ -1,6 +1,6 @@
-sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt-get update
-sudo apt-get install \
+sudo apt-get remove -y docker docker-engine docker.io containerd runc
+sudo apt-get update -y
+sudo apt-get -y install \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -10,8 +10,8 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get update -y
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 sudo groupadd docker
 sudo usermod -aG docker $USER
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
