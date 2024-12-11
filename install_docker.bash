4407fdfd2989bd1af4918f9c09dbeec860f36a93
Thiago Freitas <thiagosistemas3@gmail.com>
2024-12-11 11:08:20 +0000

    Update Docker Compose version to 1.31.0 in install_docker.bash

--- install_docker.bash
+++ install_docker.bash
@@ -14,6 +14,6 @@ sudo apt-get update -y
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 sudo groupadd docker
 sudo usermod -aG docker $USER
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.31.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
